eeba4f54a86798fc2c220085604c681d50a7586c
wxjan <janainasilva.1993@outlook.com>
2024-01-23 14:42:27 -0300

    Desafio Ruby

---
+++ desafio.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome:   "
+nome = gets.chomp
+
+print "Digite seu sobrenome:   "
+sobrenome = gets.chomp
+
+print "Digite sua idade:   "
+idade = gets.chomp
+
+puts "meu nome é #{nome} #{sobrenome} , eu tenho #{idade} anos "
